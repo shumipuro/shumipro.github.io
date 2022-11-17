a3bad51a27100eb52563de158696db548d50ecd2
shumipuro <shumipuro@gmail.com>
2022-11-17 23:52:12 +0900

    Update 22-11-17-23-52

--- JS/CMS/article.js
+++ JS/CMS/article.js
@@ -1,25 +1,19 @@
-var rank = ["<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
+var rank = [
 "<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
-"<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>"];
-
-var latest = ["<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
 "<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
-"<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>"];
+"<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>"
+];
 
-var m,n;
-if(typeof setting === 'undefined'){
-    m = -3;
-    n = -3;
-}
-else{
-    m = -6;
-    n = -20;
-}
+var latest = [
+"<li><a href=\"https://shumipro.netlify.app/contents/announce/000002/open-our-site\"><article><img width=\"300\" src=\"https://shumipro.netlify.app/image/main/noimage.png\" alt=\"記事\"><h4 class=\"post-title\">当サイトについて</h4></article></a></li>",
+"<li><a href=\"https://shumipro.netlify.app/contents/announce/000002/open-our-site\"><article><img width=\"300\" src=\"https://shumipro.netlify.app/contents/announce/000002/noimage.png\" alt=\"記事\"><h4 class=\"post-title\">当サイトについて</h4></article></a></li>",
+"<li><a href=\"https://shumipro.netlify.app/contents/announce/000002/open-our-site\"><article><img width=\"300\" src=\"C:/Users/rikum/Desktop/programs/front-end/mysite/home/contents//announce/000002/noimage.png\" alt=\"記事\"><h4 class=\"post-title\">当サイトについて</h4></article></a></li>"
+];
 
-rank.slice(m).forEach(function(createElement){
+rank.forEach(function(createElement){
     document.querySelector(".rank-article ul").insertAdjacentHTML('beforeend', createElement);
 });
-    
-latest.slice(n).forEach(function(createElement){
+
+latest.forEach(function(createElement){
     document.querySelector(".latest-article ul").insertAdjacentHTML('beforeend', createElement);
 });
