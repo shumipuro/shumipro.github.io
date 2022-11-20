92c624d68376c87d39047bf0a4fa166f1d483cbd
shumipuro <shumipuro@gmail.com>
2022-11-21 01:21:15 +0900

    Update 22-11-21-01-21

--- JS/CMS/articlelist.js
+++ JS/CMS/articlelist.js
@@ -3,7 +3,6 @@ var rank = [
 "<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
 "<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
 "<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
-"<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>",
 "<li><a href=\"https://shumipro.netlify.app/contents/announce/000001/open-our-site\"><article><img width=\"300\" src=\"contents\\announce\\000001\\making-website-blue.png\" alt=\"テスト\"><h4 class=\"post-title\">サイトを公開しました</h4></article></a></li>"
 ];
 
